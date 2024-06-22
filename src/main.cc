3762e43116cf579d8f0873593d89acca23d3394e
meersuri <meersuri@gmail.com>
2024-06-22 16:53:13 -0400

    Support passing multiple inputs/outputs

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,24 @@
 #include "subgraph_extractor.h"
 #include "onnx.proto3.pb.h"
 
+std::vector<std::string> parseNames(const std::string& names) {
+    std::vector<std::string> parsed_names;
+    std::string name;
+    for (char c: names) {
+        if (c == ',') {
+            if (!name.empty()) {
+                parsed_names.push_back(std::move(name));
+                name.clear();
+            }
+        }
+        else {
+            name.push_back(c);
+        }
+    }
+    parsed_names.push_back(std::move(name));
+    return parsed_names;
+}
+
 int main(int argc, const char* argv[]) {
     if (argc < 2) {
         throw std::runtime_error("Specify path to model");
@@ -15,7 +33,7 @@ int main(int argc, const char* argv[]) {
     }
     auto model = std::make_shared<OnnxModel>(argv[1]);
     OnnxSubgraphExtractor ex(model);
-    auto new_model = ex.extract({argv[2]}, {argv[3]});
+    auto new_model = ex.extract(parseNames(argv[2]), parseNames(argv[3]));
     new_model->save("cropped.onnx");
     return 0;
 }

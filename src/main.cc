4139715eaedaf80844e2ad9cf6c60a2b4450592e
meersuri <meersuri@gmail.com>
2024-06-23 18:36:53 -0400

    Add CLI11 for args

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,12 @@
 
 #include "subgraph_extractor.h"
 #include "onnx.proto3.pb.h"
+#include "CLI11.hpp"
 
 std::vector<std::string> parseNames(const std::string& names) {
+    if (names.empty()) {
+        return {};
+    }
     std::vector<std::string> parsed_names;
     std::string name;
     for (char c: names) {
@@ -22,18 +26,23 @@ std::vector<std::string> parseNames(const std::string& names) {
 }
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2) {
-        throw std::runtime_error("Specify path to model");
+    CLI::App app{"SubgraphExtractor!!"};
+    std::string input_names, output_names, model_path, output_path;
+    app.add_option("-f, --file", model_path, "Path to .onnx model")->required();
+    app.add_option("-i, --inputs", input_names, "Name(s) of input node(s)");
+    app.add_option("-o, --outputs", output_names, "Name(s) of output node(s)");
+    app.add_option("-n, --name", output_names, "Output model path");
+    CLI11_PARSE(app, argc, argv);
+    size_t pos = model_path.find(".onnx");
+    if (output_path.empty()) {
+        output_path = model_path.substr(0, pos) + "_subgraph.onnx";
     }
-    if (argc < 3) {
-        throw std::runtime_error("Enter inputs and outputs");
-    }
-    if (argc < 4) {
-        throw std::runtime_error("Enter outputs");
-    }
-    auto model = std::make_shared<OnnxModel>(argv[1]);
+    auto model = std::make_shared<OnnxModel>(model_path);
     OnnxSubgraphExtractor ex(model);
-    auto new_model = ex.extract(parseNames(argv[2]), parseNames(argv[3]));
-    new_model->save("cropped.onnx");
+    auto new_model = ex.extract(parseNames(input_names), parseNames(output_names));
+    if (output_path.find(".onnx") == std::string::npos) {
+        output_path += ".onnx";
+    }
+    new_model->save(output_path);
     return 0;
 }
